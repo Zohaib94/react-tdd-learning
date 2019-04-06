582238e0e72ea06bf81bef2e5325fe65275f1663
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2019-04-06 22:29:06 +0500

    Lesson 8: Rendering Lists Testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      <h2>This is a testing application</h2>
-    </div>
-  )
+class App extends Component {
+  state = {
+    gifts: []
+  }
+
+  addGift = () => {
+    let { gifts } = this.state
+    gifts.push({ id: gifts.length })
+    this.setState({ gifts })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>This is a testing application</h2>
+        <button className='add-gift' onClick={this.addGift}>
+          Add Gift
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,15 @@ const app = shallow(<App/>);
 it('is expected that component will render correctly', () => {
   expect(app).toMatchSnapshot();
 })
+
+it('is expected that component will initialize with empty array', () => {
+  expect(app.state().gifts).toEqual([])
+})
+
+it('gift will be added on clicking the `Add Gift` button', () => {
+  const previousGiftStateLength = app.state().gifts.length
+  app.find('.add-gift').simulate('click')
+
+  let isGiftAdded = (Number(app.state().gifts.length) > previousGiftStateLength)
+  expect(isGiftAdded).toBe(true);
+})
