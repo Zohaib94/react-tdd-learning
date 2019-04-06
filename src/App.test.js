97bd044dfac147300ed3e711138cf96cf3eb58e4
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2019-04-06 21:43:37 +0500

    Lesson 7: Setup Jest & Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const app = shallow(<App/>);
+
+it('is expected that component will render correctly', () => {
+  expect(app).toMatchSnapshot();
+})
