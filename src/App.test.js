c9f1e128d49fc443973705f93fa845598245882f
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2019-04-07 12:34:28 +0500

    Lesson 11: BDD Introduction

--- src/App.test.js
+++ src/App.test.js
@@ -4,26 +4,37 @@ import App from './App'
 
 const app = shallow(<App/>);
 
-it('is expected that component will render correctly', () => {
-  expect(app).toMatchSnapshot();
-})
+describe('When App is rendered', () => {
+  it('is expected that component will render correctly', () => {
+    expect(app).toMatchSnapshot();
+  })
 
-it('is expected that component will initialize with empty array', () => {
-  expect(app.state().gifts).toEqual([])
-})
+  it('is expected that component will initialize with empty array', () => {
+    expect(app.state().gifts).toEqual([])
+  })
 
-it('gift will be added on clicking the `Add Gift` button', () => {
-  const previousGiftStateLength = app.state().gifts.length
-  app.find('.add-gift').simulate('click')
-  const currentGiftStateLength = app.state().gifts.length
+  describe('When `Add Gift` is clicked', () => {
+    const previousGiftListLength = app.find('.gift-list').children().length
+    const previousGiftStateLength = app.state().gifts.length
 
-  expect(currentGiftStateLength).toBeGreaterThan(previousGiftStateLength)
-})
+    beforeEach(() => {
+      app.find('.add-gift').simulate('click')
+    })
 
-it('expects that a new gift is rendered after clicking `Add Gift Button`', () => {
-  const previousGiftListLength = app.find('.gift-list').children().length
-  app.find('.add-gift').simulate('click')
-  const currentGiftListLength = app.find('.gift-list').children().length
+    afterEach(() => {
+      app.setState({
+        gifts: []
+      })
+    })
 
-  expect(currentGiftListLength).toBeGreaterThan(previousGiftListLength)
+    it('expects that gift will be added', () => {
+      const currentGiftStateLength = app.state().gifts.length
+      expect(currentGiftStateLength).toBeGreaterThan(previousGiftStateLength)
+    })
+
+    it('expects that a new gift is rendered', () => {
+      const currentGiftListLength = app.find('.gift-list').children().length
+      expect(currentGiftListLength).toBeGreaterThan(previousGiftListLength)
+    })
+  })
 })
